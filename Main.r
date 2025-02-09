5cbacdaf2daa03547e78a13221843f091389eb4e
Puja <puja.mi.us@gmail.com>
2025-02-09 17:16:57 -0500

    added Region column

--- Main.r
+++ Main.r
@@ -6,62 +6,73 @@
 ## Programmer: Puja Ravi (International Academy)
 ########################################################
 "2025 Wharton Data Science Competition"
-format(Sys.Date(), "%c") 
-
-
+## install.packages("tidyverse") # nolint
+library(tidyverse)
+format(Sys.Date(), "%c")
 ########################################################
 ## LOAD INPUT DATA INTO DATA FRAMES
 ########################################################
 # load Regions data into data frame
 "Loading Regions Data..."
-RegionData <- read.csv("data/Regions.csv", header = TRUE, sep = ",")
+Region_Data <- read.csv("data/Regions.csv", header = TRUE, sep = ",") # nolint
 "COMPLETED Loading Regions Data..."
 # load games_2022 data into data frame
 "Loading Games Data..."
-GamesData <- read.csv("data/games_2022 v2.csv", header = TRUE, sep = ",")
+Games_Data <- read.csv("data/games_2022 v2.csv", header = TRUE, sep = ",") # nolint
 "COMPLETED Loading Games Data..."
 "Loading EastRegion Data..."
 # load EastRegion data into data frame
-EastRegionData <- read.csv("data/East Region.csv", header = TRUE, sep = ",")
+EastRegion_Data <- read.csv("data/East Region.csv", header = TRUE, sep = ",") # nolint
 "COMPLETED Loading EastRegion Data..."
 # load DataDict data into data frame
 "Loading Data Dictionary Data..."
-DataDict <- read.csv("data/Data Dictionary.csv", header = TRUE, sep = ",")
+Data_Dict <- read.csv("data/Data Dictionary.csv", header = TRUE, sep = ",") # nolint
 "COMPLETED Loading Data Dictionary Data..."
 
-# print the RegionData frame
-# print(RegionData)
-# print the GamesData frame
-# print(GamesData)
-# print the EastRegionData frame
-# print(EastRegionData)
-# print the DataDict frame
-# print(DataDict)
-
-
-#############################################################
-## Create a new Data Frame to add Regions to GameData Frame
-#############################################################
-
+## print the Region_Data frame
+## print(Region_Data) # nolint
+## print the Games_Data frame
+## print(Games_Data) # nolint
+## print the EastRegion_Data frame
+## print(EastRegion_Data) # nolint
+## print the Data_Dict frame
+## print(Data_Dict) # nolint
 # print number of columns
-print(ncol(GamesData))
+print(ncol(Games_Data))
 # print number of rows
-print(nrow(GamesData))
+print(nrow(Games_Data))
+
+
+########################################################
+## STAGING THE DATA
+########################################################
+
+## FGP_2%: Create a new column to average the Field Throw 2 pointer # nolint
+################################################################### # nolint
+# Master_temp_Data <- Games_Data %>% mutate(win = case_when(team_score > opponent_team_score ~ 1, team_score > opponent_team_score ~ 0 ), loss = case_when(team_score > opponent_team_score ~ 0, team_score > opponent_team_score ~ 1 ), pts_dif = team_score - opponent_team_score) # nolint
+
+## Region: Create a new Data Frame (Master_Data) to add Region column to GameData Frame # nolint
+######################################################################################## # nolint
 
 ## Adding a New Blank Column 'Region' to the GameData Frame
-GamesData['Region'] <- NA
+# Games_Data["region"] <- NA # nolint
 
-# print number of columns
-print(ncol(GamesData))
+## Games_Data$Region <- Region_Data[Games_Data$team == Region_Data$team] # nolint
+
+Master_Data <- merge(Games_Data, Region_Data, by="team", all = TRUE) # nolint
+
+print(ncol(Master_Data))
 # print number of rows
-print(nrow(GamesData))
+print(nrow(Master_Data))
 
-# GamesData$Region <- RegionData[GamesData$team == RegionData$team]
-
-
-
-
-write.csv(GamesData,"data/master games data.csv")
+write.csv(Master_Data, "data/master games data.csv")
 "COMPLETED CSV file written Successfully..."
+
+
+
+
+#############################################################
+## ANALYZING EACH GAME
+#############################################################
 
 "Program Ended"
